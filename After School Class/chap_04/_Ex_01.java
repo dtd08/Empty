e0cd614fd4e0460bdb98d31ed53875f43b871033
dtd08 <126937437+dtd08@users.noreply.github.com>
2023-04-26 18:12:50 +0900

    Update _Ex_01.java

--- After School Class/chap_04/_Ex_01.java
+++ After School Class/chap_04/_Ex_01.java
@@ -2,6 +2,7 @@ package chap04;
 
 public class _Ex_01 {
 	public static void main(String[] args) {
+		// 구구단 짝수단만 출력
 		for(int i = 2; i <= 9; i+=2) {
 			for(int j = 1; j <= 9; j++ ) {
 				System.out.println(i + " * " + j + " = " + i*j);
