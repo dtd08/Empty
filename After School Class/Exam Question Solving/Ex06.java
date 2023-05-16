fa2fddc1fe970657db55e9d8a0531738c257f05e
dtd08 <126937437+dtd08@users.noreply.github.com>
2023-05-16 17:48:19 +0900

    Add files via upload

---
+++ After School Class/Exam Question Solving/Ex06.java
@@ -0,0 +1,16 @@
+package exam;
+
+public class Ex06 {
+	public static void main(String[] args) {
+		// SUNGIL의 각 아ㅏㄹ파벳의 아스키코드의 합을 구하시오
+		char s = 'S';
+		char u = 'U';
+		char n = 'N';
+		char g = 'G';
+		char i = 'I';
+		char l = 'L';
+		System.out.println(s+u+n+g+i+l);
+	}
+	
+	
+}
