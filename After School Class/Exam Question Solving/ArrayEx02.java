83e3ab8d4100b7bc8e02a1436c5019c7be19d160
dtd08 <126937437+dtd08@users.noreply.github.com>
2023-06-01 17:17:23 +0900

    Update ArrayEx02.java

--- After School Class/Exam Question Solving/ArrayEx02.java
+++ After School Class/Exam Question Solving/ArrayEx02.java
@@ -24,8 +24,7 @@ public class ArrayEx02 {
 		}
 		
 		for(int i = 0; i < nums.length; i++) {
-			if((i+1) == except[i])
-				System.out.println(nums[i]);
+			System.out.println(nums[except[i-1]-1]);
 		}
 		
 	}
