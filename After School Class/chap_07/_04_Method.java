caeebc4cc984d4b674018d1551909895946739b2
dtd08 <126937437+dtd08@users.noreply.github.com>
2023-06-20 17:27:50 +0900

    Update _04_Method.java

--- After School Class/chap_07/_04_Method.java
+++ After School Class/chap_07/_04_Method.java
@@ -6,9 +6,9 @@ public class _04_Method {
 		BlackBox b1 = new BlackBox();
 		b1.modelName = "Blacky";
 		
-		b1.AutoReport();
+		b1.autoReport();
 		BlackBox.canAutoReport = true; // 자동 충돌 감지 업데이트 완료
-		b1.AutoReport();		
+		b1.autoReport();		
 		b1.insertMemoryCard(512); // 인스턴스 메소드
 		
 	}
